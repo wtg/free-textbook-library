9d2a69a46b0b593750de17dabcacb76a170c1697
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-21 04:41:33 -0400

    Checkout route blocks

--- web-client/src/modules/checkout/pages/list/index.vue
+++ web-client/src/modules/checkout/pages/list/index.vue
@@ -3,29 +3,22 @@
   <div class="container" v-else>
 
     <div class="row">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <h2>
-          <i class="fa fa-square-o"></i>
+          <i class="fa fa-square-shopping-cart"></i>
           Checkouts
         </h2>
-      </div>
-
-      <div class="col-md-4 text-right">
-        <b-button variant="primary" to="/checkouts/new">
-          <i class="fa fa-fw fa-plus"></i>
-          New Checkout
-        </b-button>
       </div>
     </div>
 
     <!-- List View -->
     <div class="row">
-      <div class="col-lg-12">
-        <SearchBar module='checkout'/>
-      </div>
-      <div class="col-lg-12">
-        <b-pagination :total-rows="totalRows" :value="currentPage" :per-page="perPage" @change="goToPage" />
-      </div>
+      <!-- <div class="col-lg-12"> -->
+        <!-- <SearchBar module='checkout'/> -->
+      <!-- </div> -->
+      <!-- <div class="col-lg-12"> -->
+        <!-- <b-pagination :total-rows="totalRows" :value="currentPage" :per-page="perPage" @change="goToPage" /> -->
+      <!-- </div> -->
       <div class="col-lg-12">
         <ListView :collection="collection" />
       </div>
--- web-client/src/modules/checkout/router.js
+++ web-client/src/modules/checkout/router.js
@@ -3,27 +3,32 @@ import CheckoutList from './pages/list'
 import CheckoutNew from './pages/new'
 import CheckoutShow from './pages/show'
 import CheckoutEdit from './pages/edit'
+import Middleware from '@/routers/middleware'
 
 const CheckoutListRoute = {
   path: '/checkouts',
-  component: CheckoutList
+  component: CheckoutList,
+  beforeEnter: Middleware.requireAuth
 }
 
 const CheckoutNewRoute = {
   path: '/checkouts/new',
-  component: CheckoutNew
+  component: CheckoutNew,
+  beforeEnter: Middleware.requireAdmin
 }
 
 const CheckoutShowRoute = {
   path: '/checkouts/:id',
   component: CheckoutShow,
-  props: true
+  props: true,
+  beforeEnter: Middleware.requireAuth
 }
 
 const CheckoutEditRoute = {
   path: '/checkouts/:id/edit',
   component: CheckoutEdit,
-  props: true
+  props: true,
+  beforeEnter: Middleware.requireAdmin
 }
 
 export default [
